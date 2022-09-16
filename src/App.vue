4e05b37ba654d2347dfb9850a85fa07273b1d00c
hao.chen <hao.chen@jutze.com.cn>
2022-09-16 16:58:09 +0800

    init Project

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const stageRef = ref(null);
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="page col">
+    <div class="action-bar row align-center justify-around"></div>
+    <div class="stage" ref="stageRef"></div>
+    <div class="picked-bar" ref="pickedRef"></div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style lang="scss" scoped>
+.page{
+  width: 100vw;
+  height: 100vh;
+  position: relative;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.stage, .picked-bar, .action-bar{
+  width: 100%;
+  position: relative;
+  &>div{
+    position: absolute;
+  }
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.action-bar{
+  height: 100px;
 }
 </style>
