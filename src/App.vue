6ca3fd20c9b70705947ee335cdc700e2cef86ced
hao.chen <hao.chen@jutze.com.cn>
2022-09-20 21:55:53 +0800

    done!

--- src/App.vue
+++ src/App.vue
@@ -50,7 +50,44 @@ const squareList = ref<Array<InstanceType<typeof square>>>([]);
 const squareBorder = ref<number>(1);
 const pickedSquareBorder = ref<number>(1);
 const level = ref<number>(3);
-const srcRowList: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const bgList = [
+  "background-image: linear-gradient(45deg, #dd5e89, #f7bb97);",
+  "background-image: linear-gradient(120deg, #e0c3fc 0%, #8ec5fc 100%);",
+  "background-image: linear-gradient(45deg, #a8edea 0%, #fed6e3 100%);",
+  "background-image: linear-gradient(45deg, #d299c2 0%, #fef9d7 100%);",
+  "background-image: linear-gradient(45deg, #9890e3 0%, #b1f4cf 100%);",
+  "background-image: linear-gradient(60deg, #abecd6 0%, #fbed96 100%);",
+  "background-image: linear-gradient(45deg, #93a5cf 0%, #e4efe9 100%);",
+  "background-image: linear-gradient(145deg, #ffe259, #ffa751);",
+  "background-image: linear-gradient(145deg, #dad299, #b0dab9);",
+  "background-image: url('https://rescdn.sudaxmt.cn/img/bg_colorful/l4.png');",
+]
+const bgIdx = randomInt(0, bgList.length - 1);
+console.log(new Date(), bgIdx);
+const bg = bgList[bgIdx];
+const srcPrefix = 'https://rescdn.sudaxmt.cn/g';
+const srcRowList: string[] = [
+  '/1.png',
+  '/2.png',
+  '/3.png',
+  '/4.png',
+  '/5.png',
+  '/6.png',
+  '/7.png',
+  // '/8.png',
+  '/9.png',
+  '/a1.png',
+  '/a2.png',
+  '/a3.png',
+  '/a4.png',
+  '/a5.png',
+  '/a6.png',
+  '/a7.png',
+  '/a8.png',
+  '/21.png',
+  '/31.png',
+  '/41.png',
+];
 const nextLevel = () => {
   if (level.value <= 7) {
     level.value += 1;
@@ -85,7 +122,9 @@ const checkCover = () => {
   }
 }
 let selectedLength = 0;
+let gameOver = ref(false);
 const init = () => {
+  gameOver.value = false;
   squareList.value = [];
   selectedLength = 0;
   const stage: Element = stageRef.value as unknown as Element;
@@ -128,7 +167,7 @@ const init = () => {
   }
   const srcAddList: string[] = [];
   while (srcAddList.length !== squareList.value.length) {
-    const idx = randomInt(0, srcRowList.length - 1 - Math.floor(Math.abs(level.value / 2 - 4)))
+    const idx = randomInt(0, srcRowList.length - 1 - (8 - level.value));
     console.log('range src', Math.floor(Math.abs(level.value / 2 - 4)));
     srcAddList.push(srcRowList[idx]);
     srcAddList.push(srcRowList[idx]);
@@ -143,6 +182,12 @@ const init = () => {
 }
 
 const handleSelectSquare = (idx: number) => {
+  if (gameOver.value) {
+    level.value = 3;
+    init();
+    return;
+  }
+  if (squareList.value[idx].covered || selectedLength == 8) { return }
   const stageWarpper: Element = stageWarpperRef.value as unknown as Element;
   const stage: Element = stageRef.value as unknown as Element;
   const pickedBar: Element = pickedRef.value as unknown as Element;
@@ -154,6 +199,7 @@ const handleSelectSquare = (idx: number) => {
       picked[i].left = i * (pickedSquareBorder.value + 10) + (pickeWarpper.clientWidth - pickedBar.clientWidth) / 2;
     }
   }
+  navigator.vibrate(10);
   if (squareList.value[idx].selected && squareList.value[idx].top_pre && squareList.value[idx].left_pre) {
     squareList.value[idx].top = squareList.value[idx].top_pre as number;
     squareList.value[idx].left = squareList.value[idx].left_pre as number;
@@ -163,7 +209,6 @@ const handleSelectSquare = (idx: number) => {
     checkCover();
     return;
   }
-  if (selectedLength == 8) { alert('fail') };
 
   const top: number = (stageWarpper.clientHeight - stage.clientHeight) / 2 + stage.clientHeight + (pickedBar.clientHeight - pickedSquareBorder.value) / 2 - 1;
   const left: number = selectedLength * (pickedSquareBorder.value + 10) + (pickeWarpper.clientWidth - pickedBar.clientWidth) / 2;
@@ -176,20 +221,20 @@ const handleSelectSquare = (idx: number) => {
   setTimeout(() => {
     const currentSrc = squareList.value[idx].src;
     const samePicked = squareList.value.filter(item => !item.deleted && item.selected && item.src === currentSrc);
-    if (samePicked.length === 3) {
+    if (samePicked.length >= 3) {
       samePicked[0].deleted = true;
       samePicked[1].deleted = true;
       samePicked[2].deleted = true;
       selectedLength -= 3;
       orderPicked();
     }
-    if (selectedLength == 8) { alert('fail') };
+    if (selectedLength == 8) { gameOver.value = true; };
     const notDeleted = squareList.value.filter(item => !item.deleted);
     if (!notDeleted || notDeleted.length === 0) {
       nextLevel();
     }
-  }, 750)
-  checkCover();
+    checkCover();
+  }, 420)
 }
 
 onMounted(() => {
@@ -197,65 +242,95 @@ onMounted(() => {
   const pickedBar: Element = pickedRef.value as unknown as Element;
   const pickeWarpper: Element = pickedWarpperRef.value as unknown as Element;
   pickedSquareBorder.value = Math.floor((pickedBar.clientWidth - 10 * 7 - (pickeWarpper.clientWidth - pickedBar.clientWidth) / 2) / 7);
-  console.log(pickedSquareBorder);
 });
 </script>
 
 <template>
-  <div class="page col align-center">
-    <div class="title bold text-center pointer w100" @click="nextLevel">{{level - 2 }}</div>
+  <div class="page col align-center" :style="bg">
+    <div class="title bold col align-center pointer w100">
+      第 {{level - 2 }} 关
+    </div>
     <div class="stage-warpper col align-center justify-center" ref="stageWarpperRef">
       <div class="stage" ref="stageRef" :style="{'--size':squareBorder+'px'}">
         <div v-for="item of squareList" :key="item.id"
-          :style="`${item.deleted?'display:none;':`top:${item.top}px;left:${item.left}px;z-index:${item.zIndex};${item.selected?`width:${pickedSquareBorder}px;height:${pickedSquareBorder}px;`:''}`}`"
-          :class="item.covered? 'covered':'pointer'" @click.prevent="handleSelectSquare(item.id)">
-          {{ item.src }}
+          :style="`${item.deleted?'display:none;':`background-image: url(${srcPrefix}${item.src});top:${item.top}px;left:${item.left}px;z-index:${item.zIndex};${item.selected?`width:${pickedSquareBorder}px;height:${pickedSquareBorder}px;`:''}`}`"
+          :class="item.covered? 'covered':''" @click.prevent="handleSelectSquare(item.id)">
         </div>
       </div>
     </div>
-    <div ref="pickedWarpperRef" class="picked-bar-warpper col align-center justify-center">
-      <div class="picked-bar row align-center" ref="pickedRef"></div>
+    <div ref="pickedWarpperRef" class="picked-bar-warpper row align-center justify-center">
+      <div class="picked-bar-border row align-center justify-center" :class="gameOver?'failed':''">
+        <div class="picked-bar row align-center" ref="pickedRef"></div>
+      </div>
     </div>
-    <div class="action-bar row align-center justify-around"></div>
+    <!-- <div class="copyright row warp align-center justify-center">
+      <div>苏ICP备19066469号-1</div>
+      <div>苏公网安备32050802011229号</div>
+      <div>Copyright © 2022 陈昊</div>
+    </div> -->
   </div>
 </template>
 
 <style lang="scss" scoped>
+.copyright {
+  font-size: 12px;
+  height: 30px;
+  margin-top: 40px;
+
+  div {
+    margin: 0 5px;
+  }
+}
+
 .page {
   width: 100vw;
+  color: #393929;
   height: 100vh;
   position: relative;
+  background-size: cover;
+  background-position: center;
+  text-shadow: 1px 1px 1px rgb(255 255 255 / 30%);
 
   .title {
-    font-size: 20px;
+    font-size: 32px;
     letter-spacing: 2px;
-    line-height: 70px;
-    height: 70px;
+    line-height: 100px;
+    height: 100px;
+    margin: 15px 0 5px 0;
   }
 
   .stage-warpper {
     width: 100vw;
-    height: calc(100vh - 100px - 80px - 70px);
-    background: greenyellow;
+    height: calc(100vh - 100px - 120px - 80px);
   }
 
   .picked-bar-warpper {
     width: 100vw;
 
+    .failed {
+      background: rgba(255, 110, 110, 0.4) !important;
+      border: 1px solid rgba(255, 0, 0, 0.8) !important;
+    }
+
+    .picked-bar-border {
+      width: 90%;
+      border-radius: 5px;
+      height: 80%;
+      background: rgba(241, 241, 241, 0.35);
+      border: 1px solid rgba(241, 241, 241, 0.8);
+    }
+
     .picked-bar {
-      width: 86%;
+      height: 80px;
+      width: 95%;
     }
   }
 }
 
-.stage,
-.action-bar {
-  width: 100%;
-  position: relative;
-}
-
 .stage {
   height: 100vw;
+  width: 100%;
+  position: relative;
 
   &>div {
     position: absolute;
@@ -266,21 +341,14 @@ onMounted(() => {
     border-radius: 4px;
     box-shadow: 0 2px 4px 0 rgb(0 0 0 / 10%),
       0 3px 10px 0 rgb(0 0 0 / 5%);
-    transition: all ease-in-out .5s;
+    transition: all ease-in-out .35s;
+    overflow: hidden;
+    background-size: cover;
+    background-position: center;
   }
 
   .covered {
-    background: rgb(220, 220, 220) !important;
+    filter: brightness(60%);
   }
-}
-
-.action-bar {
-  height: 80px;
-  background: blueviolet;
-}
-
-.picked-bar {
-  background: salmon;
-  height: 100px;
 }
 </style>
